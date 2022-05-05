b6235e5e12b6e6bf640fe88f8e08db634e99af9b
Alexandre Brandão Lustosa <alexandre.brandao.lustosa@gmail.com>
2022-05-05 13:52:10 -0300

    ajustes

--- price.js
+++ price.js
@@ -1,12 +1,18 @@
 const { Spot } = require('@binance/connector')
 const moment = require('moment')
-const fs = require('fs')
+const fs = require('fs');
+const { exit } = require('process');
 
 const apiKey = process.env.BINANCE_API_KEY;
 const apiSecret = process.env.BINANCE_API_SECRET;
 
 const client = new Spot(apiKey, apiSecret);
 const current = moment().utc().format('YYYYMMDD_HHmmss_SSS');
+
+if (!fs.existsSync("/crypto/conf/symbols.txt")) {
+    console.log("Config file not found");
+    exit();
+}
 
 fs.readFileSync('/crypto/conf/symbols.txt', 'utf8')
     .split(';').forEach(function(symbol){
